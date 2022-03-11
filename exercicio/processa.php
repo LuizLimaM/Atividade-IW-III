b53c3fb3359534ff4a71ec17e83e75d985862149
Luiz Henrique da Silva Lima <79856769+LuizLimaM@users.noreply.github.com>
2022-03-11 12:45:12 -0300

    Add files via upload

--- exercicio/processa.php
+++ exercicio/processa.php
@@ -6,28 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
 
-
     <style>
-    p{
-        
-        color:red;
-    
-    
+
+    body{
+        background: rgb(2,0,36);
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(93,60,154,1) 48%, rgba(0,212,255,1) 100%);
+        color: #f8f8f8;
+        font-weight: bold;
+        font-size: 1em;
+        font-family: arial;
     }
-    
-    
+       
     </style>
+
 </head>
 <body>
 
 <?php
     $num1 = $_REQUEST['num1'];
     $num2 = $_REQUEST['num2'];
+    $num3 = $_REQUEST['num3'];
 
-    $soma = $num1 + $num2;
+    $soma = $num1 + $num2 + $num3;
     
+    $media = $soma /3;
 
-    echo "<p> A soma é: $soma </p>";
+    echo "<p> A média é: $media </p>";
     //echo "A soma é: ".$soma;
 
 
